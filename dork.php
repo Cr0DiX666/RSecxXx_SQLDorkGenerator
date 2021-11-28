8b1d4417bc4b14115b66db35147b334263560103
®SecxXx.iD <68939380+RSecxXx@users.noreply.github.com>
2021-11-28 18:16:14 +0700

    lattes dork.php

---
+++ dork.php
@@ -0,0 +1,83 @@
+<?php
+$green  = "\e[92m";
+$red    = "\e[91m";
+$yellow = "\e[93m";
+$blue   = "\e[36m";
+$white = " \e[37m";
+
+echo"$yellow
+   ================================================== =============
+  = $green DORK GENERATE v.1.0 - Selamat Dorking ^_^ $kuning =
+  = $biru RSecx[X]x.iD@H4x0r + XbumbbleB33 $kuning =
+  = $merah INDONESIA-SYNDICATE666Gh0sT $kuning =
+  = SQL Dork =
+  = $putih https://indonesiasyndicate666ghost.blogspot.com $kuning =
+  ================================================== ============= 
+ ================================================== =================
+ = $putih INDONESIA-SYNDICATE666GH0ST$kuning =
+ =$putih BLACKHAT HACKER'S & GRAYHAT HACKER'S$kuning =
+ =$putih INDOS666GH0ST$kuning =
+ = $putih http://indonesiasyndicate666ghost.blogspot.com/ $kuning =
+ ================================================== ================= ";
+sleep(2);
+@header('Content-Type: text/html; charset=UTF-8');
+function input($echo) {
+    echo "\n$echo --> ";
+}
+input("$blue Input 1");
+$txt1=trim(fgets(STDIN));
+
+input(" Input 2");
+$txt2=trim(fgets(STDIN));
+
+input(" Country(site)");
+$txt3=trim(fgets(STDIN));
+
+input(" Generate Now?? (y/n)");
+$jumlah=trim(fgets(STDIN));
+
+echo 
+"\n$white===========================> ^_^ <============================\n";
+if($jumlah == "y") {
+	$nama = $txt1;
+	$id = $txt2;
+	$country = $txt3;
+	echo "\n$nama.$id?id= site:$country\n";
+	sleep(1);
+	echo "$nama.$id?cat_ID= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?id_category= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?category_id= site:$country \n";
+	sleep(1);
+    echo "$nama.$id?catID= site:$country \n";
+    sleep(1);
+	echo "$nama.$id?list= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?list_id= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?r= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?login= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?member= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?admin= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?pay= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?buy= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?ID= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?reffID= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?job= site:$country \n";
+	sleep(1);
+	echo "$nama.$id?cID= site:$country \n";
+	sleep(1);
+    echo "$nama.$id?gameid= site:$country \n";
+    sleep(1);
+    echo "$id?CoID= site:$country \n";
+}
+?>
